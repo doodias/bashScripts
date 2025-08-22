ae0a34f5675dfa48fc17a29fadd3420559464093
Eduardo Dias <eduardoespd@outlook.com>
2025-08-22 20:25:43 -0300

    Script de intalação powerShell via Snap

---
+++ powerShellInstallFedora.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Instalção do suporte a pacotes Snap
+sudo dnf install snapd 
+
+sudo ln -s /var/lib/snapd/snap /snap 
+
+# Instalção do PowerShell
+sudo snap install powershell --classic
